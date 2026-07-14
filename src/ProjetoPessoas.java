6e89a956ca13d46de58e93632d622251a52a5df8
JulianoPereiraChagas <chagas.julianopereira@gmail.com>
2026-07-14 15:58:12 -0300

    Arquivo README

--- src/ProjetoPessoas.java
+++ src/ProjetoPessoas.java
@@ -1,15 +1,27 @@
 public class ProjetoPessoas {
     public static void main(String[] args) {
 
+        // Pessoas e o que elas são
+
         Pessoa p1 = new Pessoa();
         Aluno p2 = new Aluno();
         Professor p3 = new Professor();
         Funcionario p4 = new Funcionario();
 
+        // Nomes das pessoas
+
         p1.setNome("Pedro");
         p2.setNome("Maria");
         p3.setNome("Claudio");
         p4.setNome("Fabiano");
+
+
+        // Mostrando os status
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
 
         }
 
